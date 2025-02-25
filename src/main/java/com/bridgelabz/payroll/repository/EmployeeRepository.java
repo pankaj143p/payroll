6016efb5a01b8649e5c0b8bb0844ef437576faa5
pankaj143p <pankaj114477pankaj@gmail.com>
2025-02-25 10:17:10 +0530

    [Pankaj] : used sql database

--- src/main/java/com/bridgelabz/payroll/repository/EmployeeRepository.java
+++ src/main/java/com/bridgelabz/payroll/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.bridgelabz.payroll.repository;
 import com.bridgelabz.payroll.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// using sql database
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 }
 
